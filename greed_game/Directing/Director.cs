3cd9ae4cca76ca3d10f4516130f2140d09b529da
annaarutyunova <anna.arutyunova8848@gmail.com>
2022-06-15 14:59:24 -0600

    add more actors on the screen when score reaches 100

--- greed_game/Directing/Director.cs
+++ greed_game/Directing/Director.cs
@@ -5,6 +5,7 @@ using Unit04.Game.Services;
 
 namespace Unit04.Game.Directing
 {
+
     /// <summary>
     /// <para>A person who directs the game.</para>
     /// <para>
@@ -13,9 +14,23 @@ namespace Unit04.Game.Directing
     /// </summary>
     public class Director
     {
+        private static int FRAME_RATE = 12;
+        private static int MAX_X = 900;
+        private static int MAX_Y = 600;
+        private static int CELL_SIZE = 15;
+        private static int FONT_SIZE = 15;
+        private static int COLS = 60;
+        private static int ROWS = 40;
+        private static string CAPTION = "Robot Finds Kitten";
+        private static string DATA_PATH = "Data/messages.txt";
+        private static Color WHITE = new Color(255, 255, 255);
+        private static int DEFAULT_ARTIFACTS = 5;
+
         private KeyboardService keyboardService = null;
         private VideoService videoService = null;
         private int score = 0;
+        
+        int level = 1;
 
         /// <summary>
         /// Constructs a new instance of Director using the given KeyboardService and VideoService.
@@ -64,6 +79,7 @@ namespace Unit04.Game.Directing
         /// Updates the robot's position and resolves any collisions with artifacts.
         /// </summary>
         /// <param name="cast">The given cast.</param>
+        
         private void DoUpdates(Cast cast)
         {
             Actor banner = cast.GetFirstActor("banner");
@@ -75,7 +91,7 @@ namespace Unit04.Game.Directing
             int maxY = videoService.GetHeight();
             robot.MoveNext(maxX, maxY);
 
-            Random rand = new Random();
+            Random random = new Random();
             foreach (Actor actor in artifacts)
             {
                 actor.MoveNext(maxX, maxY);
@@ -84,13 +100,73 @@ namespace Unit04.Game.Directing
                     // detect collision - add or subtract points
                     Artifact artifact = (Artifact) actor;
                     UpdateScore(artifact.GetPoints());
-                    Point p = new Point(rand.Next(0,61), 0);
+                    Point p = new Point(random.Next(0,61), 0);
                     p = p.Scale(15);
                     artifact.SetPosition(p);
                     string message = score.ToString();
                     banner.SetText(message);
                 }
             } 
+
+            if (score > level * 100)
+            {
+                 for (int i = 0; i < DEFAULT_ARTIFACTS; i++)
+            {
+                string text = "*";
+                string message = "";
+
+                int x = random.Next(1, COLS);
+                int y = random.Next(1, ROWS);
+                Point position = new Point(x, y);
+                Point velocity = new Point(0, 1);
+                velocity = velocity.Scale(CELL_SIZE);
+                position = position.Scale(CELL_SIZE);
+
+                int r = random.Next(0, 256);
+                int g = random.Next(0, 256);
+                int b = random.Next(0, 256);
+                Color color = new Color(r, g, b);
+
+                Artifact artifact = new Artifact();
+                artifact.SetText(text);
+                artifact.SetFontSize(FONT_SIZE);
+                artifact.SetColor(color);
+                artifact.SetPosition(position);
+                artifact.SetVelocity(velocity);
+                artifact.SetMessage(message);
+                cast.AddActor("artifacts", artifact);
+            }
+
+                for (int i = 0; i < DEFAULT_ARTIFACTS; i++)
+            {
+                string text = "o";
+                string message = "";
+
+                int x = random.Next(1, COLS);
+                int y = random.Next(1, ROWS);
+                Point position = new Point(x, y);
+                Point velocity = new Point(0, 1);
+                velocity = velocity.Scale(CELL_SIZE);
+                position = position.Scale(CELL_SIZE);
+
+                int r = random.Next(0, 256);
+                int g = random.Next(0, 256);
+                int b = random.Next(0, 256);
+                Color color = new Color(r, g, b);
+
+                Artifact artifact = new Artifact();
+                artifact.SetText(text);
+                artifact.SetFontSize(FONT_SIZE);
+                artifact.SetColor(color);
+                artifact.SetPosition(position);
+                artifact.SetVelocity(velocity);
+                artifact.SetMessage(message);
+                artifact.SetPoints(-25);
+                cast.AddActor("artifacts", artifact);
+            }
+            level ++;
+
+            }
         }
 
         /// <summary>
